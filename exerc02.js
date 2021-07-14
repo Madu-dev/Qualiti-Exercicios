f67709f2dbadf83af5d084809479f0dfeba3394a
Maria do Carmo <69973218+Madu-dev@users.noreply.github.com>
2021-07-13 23:24:56 -0300

    Add files via upload

---
+++ exerc02.js
@@ -0,0 +1,91 @@
+// 1
+
+const nome = "Maria";
+
+//2
+
+let tempo = false;
+
+//3
+
+const dataImportante = "10/01";
+
+//4
+
+let idade = 19;
+idade = 5;
+
+//5
+
+let listaNumerica = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//6
+
+const aluno = {
+  nome: "Carlos",
+  idade: 28,
+  turno: "manhã",
+};
+
+//7
+
+let objeto = {
+  nome: nome,
+  clima: tempo,
+  data: dataImportante,
+  num: idade,
+};
+
+//8
+
+let x = 10;
+x *= 9;
+
+//9
+
+let aluno1 = {
+  nome: "Ailton",
+  idade: 19,
+  turno: "manhã",
+}
+
+let aluno2 = {
+  nome: "Carlos",
+  idade: 25,
+  turno: "manhã",
+}
+
+let aluno3 = {
+  nome: "Daniela",
+  idade: 22,
+  turno: "manhã",
+
+}
+
+//10
+
+let num = 10;
+num *= 5;
+
+
+console.log(nome + " || " +  tempo + " || " + dataImportante + " || " + idade);
+console.log();
+console.log("Lista Numérica: " + listaNumerica);
+console.log();
+console.log(aluno);
+console.log();
+console.log(aluno1);
+console.log();
+console.log(aluno2);
+console.log();
+console.log(aluno3);
+console.log();
+console.log(objeto);
+// 8 e 10
+console.log();
+console.log("Multiplicações:");
+console.log();
+console.log("X=10 multiplicado por 9 é:  " + x);
+console.log();
+console.log("num=10 multiplicado por 5 é:  " + num);
+console.log();
