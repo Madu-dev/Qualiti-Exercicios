06ecfc90b5b1889813bb9ebfa3288f34f5279573
Maria do Carmo <69973218+Madu-dev@users.noreply.github.com>
2021-07-13 23:19:39 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,91 @@
-// item 1
+// 1
 
-const lista = ["Mustang - Ford", "Panamera - Porsche"]; // item 2
+const nome = "Maria";
 
-class Automovel {
-  constructor(nome, marca, modelo, paisOrigem){
-    this.nome = nome;
-    this.marca = marca;
-    this.modelo = modelo;
-    this.paisOrigem = paisOrigem;
-  }
+//2
 
-  getAutomovel (){ 
-    return `   nome: ${this.nome} || marca: ${this.marca}
-   modelo: ${this.modelo} || Pais de Origem: ${this.paisOrigem}`;
-  }
+let tempo = false;
+
+//3
+
+const dataImportante = "10/01";
+
+//4
+
+let idade = 19;
+idade = 5;
+
+//5
+
+let listaNumerica = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//6
+
+const aluno = {
+  nome: "Carlos",
+  idade: 28,
+  turno: "manhã",
+};
+
+//7
+
+let objeto = {
+  nome: nome,
+  clima: tempo,
+  data: dataImportante,
+  num: idade,
+};
+
+//8
+
+let x = 10;
+x *= 9;
+
+//9
+
+let aluno1 = {
+  nome: "Ailton",
+  idade: 19,
+  turno: "manhã",
 }
 
-for (const valor of lista) { //item 3
-  console.log(valor);
-
-  for (const key in Automovel) {
-    console.log(key, Automovel[key]);
-  }  
+let aluno2 = {
+  nome: "Carlos",
+  idade: 25,
+  turno: "manhã",
 }
 
-var automovel = new Automovel("supra", "Toyota", "MK4", "Japão");
+let aluno3 = {
+  nome: "Daniela",
+  idade: 22,
+  turno: "manhã",
 
-console.log(automovel.getAutomovel());
+}
+
+//10
+
+let num = 10;
+num *= 5;
+
+
+console.log(nome + " || " +  tempo + " || " + dataImportante + " || " + idade);
 console.log();
-
-
+console.log("Lista Numérica: " + listaNumerica);
+console.log();
+console.log(aluno);
+console.log();
+console.log(aluno1);
+console.log();
+console.log(aluno2);
+console.log();
+console.log(aluno3);
+console.log();
+console.log(objeto);
+// 8 e 10
+console.log();
+console.log("Multiplicações:");
+console.log();
+console.log("X=10 multiplicado por 9 é:  " + x);
+console.log();
+console.log("num=10 multiplicado por 5 é:  " + num);
+console.log();
